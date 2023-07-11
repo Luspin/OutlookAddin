8294056dcad82b44cce53bb5277562ce318d7ac2
Luís Pinto <Luspin@microsoft.com>
2023-07-11 14:22:58 +0100

    Moved JS to main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+Office.onReady((info) => {
+    if (info.host === Office.HostType.Outlook) {
+        document.getElementById("helloButton").onclick = sayHello;
+    }
+});
+
+/**
+ * Writes 'Hello world!' to a new message body.
+ */
+function sayHello() {
+    Office.context.mailbox.item.body.setAsync(
+        "Hello world!",
+        {
+            coercionType: "html", // Write text as HTML
+        },
+
+        // Callback method to check that setAsync succeeded
+        function (asyncResult) {
+            if (asyncResult.status ==
+                Office.AsyncResultStatus.Failed) {
+                write(asyncResult.error.message);
+            }
+        }
+    );
+}
