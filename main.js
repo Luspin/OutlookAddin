1fd61637be50144d89cfdb60b3f262a92b0525a2
Luís Pinto <Luspin@microsoft.com>
2023-11-20 14:18:30 +0000

    promptBeforeOpen: false

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ let dialog; // Declare dialog as global for use in later functions.
 function openDialog() {
     console.log("Opening dialog");
 
-    Office.context.ui.displayDialogAsync('https://luspin.github.io/OutlookAddin/myDialog.html', {height: 20, width: 10, promptBeforeOpen: true},
+    Office.context.ui.displayDialogAsync('https://luspin.github.io/OutlookAddin/myDialog.html', {height: 20, width: 10, promptBeforeOpen: false},
     function (asyncResult) {
         dialog = asyncResult.value;
         dialog.addEventHandler(Office.EventType.DialogMessageReceived, (arg) => {
